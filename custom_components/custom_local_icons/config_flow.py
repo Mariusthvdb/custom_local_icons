93e0ebf23384b97e267898073881045701789ff4
Marius <33354141+Mariusthvdb@users.noreply.github.com>
2026-05-30 14:21:13 +0200

    Update config_flow.py

--- custom_components/custom_local_icons/config_flow.py
+++ custom_components/custom_local_icons/config_flow.py
@@ -61,17 +61,3 @@ class CustomLocalIconsConfigFlow(
             data_schema=schema,
             errors=errors,
         )
-
-# import logging
-#
-# from homeassistant import config_entries
-#
-# _LOGGER = logging.getLogger(__name__)
-#
-#
-# @config_entries.HANDLERS.register("custom_local_icons")
-# class FontawesomeConfigFlow(config_entries.ConfigFlow):
-#     async def async_step_user(self, user_input=None):
-#         if self._async_current_entries():
-#             return self.async_abort(reason="single_instance_allowed")
-#         return self.async_create_entry(title="", data={})
