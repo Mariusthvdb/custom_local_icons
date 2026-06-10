3772a2133a82411b3e0f3e1e5a556288b0c272f2
Marius <33354141+Mariusthvdb@users.noreply.github.com>
2026-06-10 15:35:16 +0200

    Update const.py

--- custom_components/custom_local_icons/const.py
+++ custom_components/custom_local_icons/const.py
@@ -5,8 +5,6 @@ DOMAIN = "custom_local_icons"
 NAME = "Custom Local Icons"
 URL = "https://github.com/Mariusthvdb/custom_local_icons"
 
-VERSION = "1.3.1"
-
 # Frontend loader
 LOADER_URL = f"/{DOMAIN}/main.js"
 LOADER_PATH = f"custom_components/{DOMAIN}/main.js"
@@ -19,3 +17,8 @@ ICONLIST_URL = f"/{DOMAIN}/list"
 
 # Info endpoint (important for main.js + debugging)
 INFO_URL = f"/{DOMAIN}/info"
+
+OPTION_FRONTEND_DEBUG = "frontend_debug"
+DEFAULT_FRONTEND_DEBUG = False
+
+VERSION = "1.4.0"
